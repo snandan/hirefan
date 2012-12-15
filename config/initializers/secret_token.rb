b7310fd5ac17db60db30eff3dac99e95d9a62c27
Suman Nandan <suman.nandan@gmail.com>
2012-12-15 20:30:45 +0530

    Invoked 'rails new hirefan'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hirefan::Application.config.secret_token = '53e244c9e8fb70a6071ae2c42513ed7a4474c70a0dbee23a85df171986e0fe598b54f319863f07ce8dbd6e18e472ccf03e3a9e0eb5c4547fb912db07f6457815'
